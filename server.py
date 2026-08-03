357eff4425f7685df34e4f093c98ed87de561411
Hermes3x <jonathan.zvara@gmail.com>
2026-08-03 21:23:20 +0200

    fix: gestion des emails inconnus dans showSummary (closes #1)

--- server.py
+++ server.py
@@ -26,8 +26,16 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+
+    matching_clubs = [club for club in clubs if club['email'] == request.form['email']]
+
+    if not matching_clubs:
+        flash("Sorry, that email wasn't found.")
+        return render_template('index.html')
+
+    else :
+        club = matching_clubs[0]
+        return render_template('welcome.html',club=club,competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
---
+++ tests/integration/test_login.py
@@ -0,0 +1,12 @@
+def test_unknown_email_does_not_crash(client):
+    invalid_mail = "inconnu@test.com"
+    response = client.post("/showSummary", data={"email": invalid_mail})
+
+    assert response.status_code == 200
+
+
+def test_unknown_email_shows_error_message(client):
+    invalid_mail = "inconnu@test.com"
+    response = client.post("/showSummary", data={"email": invalid_mail})
+    
+    assert "Sorry, that email wasn" in response.get_data(as_text=True)
